9f861b8b0eecdcf72df47919886648b759aac275
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-09-02 12:07:08 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 REM 激活虚拟环境
-.venv\Scripts\python.exe  .\src\imecas_auto_reconnect\gui.py
+cd src\imecas_auto_reconnect
+..\..\.venv\Scripts\python.exe .\gui.py
 REM 停留在命令提示符窗口，直到用户按下任意键
 pause
